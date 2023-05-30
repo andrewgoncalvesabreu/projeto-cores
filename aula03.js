9569502e9b865f8fe68f546da4ef458af7959316
Andrew Gonçalves Abreu <124524087+andrewgoncalvesabreu@users.noreply.github.com>
2023-05-30 11:52:27 -0300

    Cores

---
+++ aula03.js
@@ -0,0 +1,11 @@
+document.getElementById('btn-laranja').addEventListener('click', function(){
+    document.querySelector('body').setAttribute("class" , "laranja")
+});
+
+document.getElementById('btn-vermelho').addEventListener('click' , function(){
+    document.querySelector('body').setAttribute("class" , "vermelho")
+})
+
+document.getElementById('btn-azul').addEventListener('click' , function(){
+    document.querySelector('body').setAttribute("class" , "azul")
+});
